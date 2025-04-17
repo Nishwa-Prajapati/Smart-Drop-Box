c1d18a362050be228501010ec20bcda9d57725aa
Nishwa-Prajapati <nishwa.prajapati@gmail.com>
2025-04-17 14:50:31 +0530

    Update aaguidancepage.js

--- Frontend/SCRIPT/aaguidancepage.js
+++ Frontend/SCRIPT/aaguidancepage.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let userBtn = document.getElementById("user-btn");
+    let deliveryBtn = document.getElementById("delivery-btn");
+    let userInfo = document.getElementById("user-info");
+    let deliveryInfo = document.getElementById("delivery-info");
 
+    
+    userBtn.addEventListener("click", function () {
+        userInfo.classList.add("show");
+        deliveryInfo.classList.remove("show"); 
+    });
+
+    
+    deliveryBtn.addEventListener("click", function () {
+        deliveryInfo.classList.add("show");
+        userInfo.classList.remove("show"); 
+    });
+});
