f00285162c5355e357b687f435bf9084a53518c8
Nishwa-Prajapati <nishwa.prajapati@gmail.com>
2025-04-17 15:02:09 +0530

    Update authRoutes.js

--- backend/ROUTES/authRoutes.js
+++ backend/ROUTES/authRoutes.js
@@ -1,1 +1,17 @@
+const express = require("express");
+// const { signup, login } = require("../controllers/authController");
+// const { setOrderId } = require("../controllers/authController");
+const { signup, login, setOrderId, deleteDeliveryGuyAndOtps } = require("../controllers/authController");
+const authenticateUser = require("../middleware/authMiddleware");
 
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+
+console.log("setOrderId:", setOrderId);
+router.post("/set-order", authenticateUser, setOrderId);
+
+router.delete("/delete-delivery", deleteDeliveryGuyAndOtps);
+
+module.exports = router;
